9fc642398fcfe20a6f024a30cc02e479b50d0e51
maxshmax666 <maxshirakov@gmail.com>
2025-08-24 10:25:06 +0500

    feat: load env vars at build time

--- lib/shared/constants/app_strings.dart
+++ lib/shared/constants/app_strings.dart
@@ -2,10 +2,6 @@
 class AppStrings {
   AppStrings._();
 
-  static const String supabaseUrl = 'https://asoyjqtqtomxcdmsgehx.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzb3lqcXRxdG9teGNkbXNnZWh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxMDc2NzIsImV4cCI6MjA3MDY4MzY3Mn0.AgVnUEmf4dO3aaVBJjZ1zJm0EFUQ0ghENtpkRqsXW4o';
-
   static const String profileCar = 'driving-car';
   static const String profileFoot = 'foot-walking';
   static const String profileBike = 'cycling-regular';
@@ -16,3 +12,4 @@ class AppStrings {
 
   static const String appTitle = 'GreenWave';
 }
+
